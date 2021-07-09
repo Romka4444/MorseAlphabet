43132eb8e1ed68fb5223ff175b0f27ba2b70f011
Roman <roman.sevruk@partida.io>
2021-07-09 07:56:20 +0300

    HashMap is created

--- src/main/java/MorzeAzbuka.java
+++ src/main/java/MorzeAzbuka.java
@@ -1,2 +1,39 @@
+import java.util.HashMap;
+
+/*
+Encoding from characters to morze code
+ */
+
 public class MorzeAzbuka {
+    public static void main(String[] args) {
+
+        HashMap<String, String> morze = new HashMap<String, String>();
+        morze.put("A", ". -");
+        morze.put("B", "- . . .");
+        morze.put("C", "- . - .");
+        morze.put("D", "- . .");
+        morze.put("E", ".");
+        morze.put("F", ". . - .");
+        morze.put("G", "- - .");
+        morze.put("H", ". . . .");
+        morze.put("I", ". .");
+        morze.put("J", ". - - -");
+        morze.put("K", "- . -");
+        morze.put("L", ". - . .");
+        morze.put("M", "- -");
+        morze.put("N", "- .");
+        morze.put("O", "- - -");
+        morze.put("P", ". - - .");
+        morze.put("Q", "- - . -");
+        morze.put("R", ". - .");
+        morze.put("S", ". . .");
+        morze.put("T", "-");
+        morze.put("U", ". . -");
+        morze.put("V", ". . . -");
+        morze.put("W", ". - -");
+        morze.put("X", "- . . -");
+        morze.put("Y", "- . - -");
+        morze.put("Z", "- - . .");
+
+    }
 }
